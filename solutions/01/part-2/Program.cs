d425fa8e8741d65e0ea0e5b3b274d8de5b8c0f25
Rob Habraken <roberthabraken@hotmail.com>
2025-12-01 13:28:18 +0100

    Clean up day 1 solutions

--- solutions/01/part-2/Program.cs
+++ solutions/01/part-2/Program.cs
@@ -1,4 +1,4 @@
-string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2025-io\\01\\input.txt");
+var lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2025-io\\01\\input.txt");
 
 var answer = 0;
 var dial = 50;
@@ -6,30 +6,23 @@ var dial = 50;
 foreach (var line in lines)
 {
     var rotations = int.Parse(line[1..]);
-
-    answer += rotations / 100; // when the number of rotations is higher than 100, for every 100 rotations we count one time passing zero
-    rotations %= 100; // only rotate what's left now
-
-    if (line[0].Equals('L'))
-        rotations *= -1;
+    answer += rotations / 100;
+    rotations %= 100;
+    if (line[0].Equals('L')) rotations *= -1;
 
     dial += rotations;
-
     if (dial < 0)
     {
-        if (dial != rotations) // if we don't leave from the dial point at zero and the number of rotations exceeds the current dial position when turning left
-            answer++;
+        if (dial != rotations) answer++;
         dial += 100;
     }
     else if (dial >= 100)
     {
-        if (dial > 100)
-            answer++; // if we go past 0 when turning right
+        if (dial > 100) answer++;
         dial -= 100;
     }
 
-    if (dial == 0)
-        answer++; // if we end on zero
+    if (dial == 0) answer++;
 }
 
 Console.WriteLine(answer);
