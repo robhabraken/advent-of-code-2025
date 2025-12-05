e3aacf62b05f14f969ef171d8d3d23d79980a6f0
Rob Habraken <roberthabraken@hotmail.com>
2025-12-05 15:59:04 +0100

    Clean up day 5 part 2

--- solutions/05/part-2/Program.cs
+++ solutions/05/part-2/Program.cs
@@ -1,42 +1,38 @@
 var lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2025-io\\05\\input.txt");
 
 var answer = 0L;
-var ranges = new List<Range>();
+var ranges = new List<(long from, long to)>();
 
 foreach (var line in lines)
 {
     if (string.IsNullOrEmpty(line)) break;
 
     var range = line.Split('-').Select(long.Parse).ToArray();
-    ranges.Add(new Range(range[0], range[1]));
+    ranges.Add((range[0], range[1]));
 }
 
-while (merge(ref ranges));
+while (merge(ref ranges)) ;
 
-foreach (var range in ranges.Distinct())
-    answer += range.to - range.from + 1;
+foreach (var (from, to) in ranges.Distinct())
+    answer += to - from + 1;
 
 Console.WriteLine(answer);
 
-bool merge(ref List<Range> ranges)
+bool merge(ref List<(long from, long to)> ranges)
 {
     var modified = false;
-    var result = new List<Range>();
+    var result = new List<(long from, long to)>();
     foreach (var range in ranges)
     {
         var overlaps = false;
         foreach (var compare in ranges)
-        {
-            if (!range.Equals(compare))
-            {
-                if (isOverlapping(range, compare))
+            if (!range.Equals(compare) && isOverlapping(range, compare))
                 {
                     overlaps = modified = true;
-                    result.Add(new Range(Math.Min(range.from, compare.from), Math.Max(range.to, compare.to)));
+                    result.Add((Math.Min(range.from, compare.from), Math.Max(range.to, compare.to)));
                     break;
                 }
-            }
-        }
+
         if (!overlaps)
             result.Add(range);
     }
@@ -44,19 +40,6 @@ bool merge(ref List<Range> ranges)
     return modified;
 }
 
-bool isOverlapping(Range a, Range b) => (a.from >= b.from && a.from <= b.from && a.to > b.to) ||
-                                        (a.from < b.from && a.to <= b.to && a.to >= b.from) ||
-                                        (a.from < b.from && a.to > b.to) ||
-                                        (a.from >= b.from && a.to <= b.to);
-
-struct Range
-{
-    public long from;
-    public long to;
-
-    public Range(long from, long to)
-    {
-        this.from = from;
-        this.to = to;
-    }
-}
+bool isOverlapping((long from, long to) a, (long from, long to) b) => (a.from >= b.from && a.from <= b.from && a.to > b.to) ||
+                                                                        (a.from < b.from && a.to <= b.to && a.to >= b.from) ||
+                                                                        (a.from < b.from && a.to > b.to) || (a.from >= b.from && a.to <= b.to);
