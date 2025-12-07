478183e62c40d8edf727ad8e2add46df4455cf6d
Rob Habraken <roberthabraken@hotmail.com>
2025-12-07 21:32:15 +0100

    Performance improvement for day 7 part 2 switching from a Dictionary to an array

--- solutions/07/part-2/Program.cs
+++ solutions/07/part-2/Program.cs
@@ -1,31 +1,20 @@
 var lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2025-io\\07\\input.txt");
 
-var beams = new Dictionary<int, long> { { lines[0].IndexOf('S'), 1 } };
+var beams = new long[lines.Length + 1, lines[0].Length];
+beams[0, lines[0].IndexOf('S')] = 1;
 
-for (var y = 2; y < lines.Length; y += 2)
-{
-    var newBeams = new Dictionary<int, long>();
-    foreach (var x in beams.Keys)
-    {
+for (var y = 0; y < lines.Length; y += 2)
+    for (var x = 0; x < lines[0].Length; x++)
         if (lines[y][x].Equals('^'))
         {
-            set(newBeams, x - 1, beams[x]);
-            set(newBeams, x + 1, beams[x]);
+            beams[y + 2, x - 1] += beams[y, x];
+            beams[y + 2, x + 1] += beams[y, x];
         }
         else
-            set(newBeams, x, beams[x]);
-    }
-    beams = newBeams;
-}
+            beams[y + 2, x] += beams[y, x];
 
 var timelines = 0L;
-foreach (var timeline in beams.Keys)
-    timelines += beams[timeline];
+for (var x = 0; x < lines[0].Length; x++)
+    timelines += beams[lines.Length, x];
 
 Console.WriteLine(timelines);
-
-void set(Dictionary<int, long> target, int index, long value)
-{
-    if (!target.TryAdd(index, value))
-        target[index] += value;
-}
