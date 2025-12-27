497e86896ae3012d7b34107cc5f3aa30d94d5f9b
Rob Habraken <roberthabraken@hotmail.com>
2025-12-27 22:46:27 +0100

    Small performance upgrade on day 2 part 2

--- solutions/02/part-2/Program.cs
+++ solutions/02/part-2/Program.cs
@@ -1,6 +1,7 @@
 var lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2025-io\\02\\input.txt");
 
 var invalidIDs = 0L;
+
 foreach (var range in lines[0].Split(','))
 {
     var edges = range.Split('-').Select(long.Parse).ToArray();
@@ -11,6 +12,11 @@ foreach (var range in lines[0].Split(','))
             if (strId.Replace(strId[0..i], string.Empty).Length == 0)
             {
                 invalidIDs += id;
+
+                // skip to the increment of the pattern found if possible
+                if (i >= 2 && strId[0..i].Distinct().Count() > 2)
+                    id = long.Parse($"{int.Parse(strId[0..i]) + 1}".PadRight(strId.Length, '0'));
+
                 break;
             }
     }
